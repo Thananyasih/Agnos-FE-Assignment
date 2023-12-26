3d3e5b2ea53697321bed1182c0bc1a7e76177ac9
Thananyasih <thananya.sih@gmail.com>
2023-12-26 22:29:36 +0700

    edit screen size for abs page

--- src/components/AbsSelectPain.jsx
+++ src/components/AbsSelectPain.jsx
@@ -39,7 +39,7 @@ const AbsSelectPain = () => {
         <div  className='container flex flex-row absolute'>
             {!topAbsSelected ? (<img src={PainsData.absPain.absImg.absTop} alt="Epigastrium Pain" className='relative opacity-0'/>) : 
             (<div>
-                <img src={PainsData.absPain.absMes.absTopMes} alt='Epigastrium Pain'className='absolute left-[10px]'/>
+                <img src={PainsData.absPain.absMes.absTopMes} alt='Epigastrium Pain'className='absolute left-[10px] fold:left-[0px]'/>
                 <img src={PainsData.absPain.absImg.absTop} alt="Epigastrium Pain" className='relative' /> 
             </div>)}
         </div>
@@ -63,7 +63,7 @@ const AbsSelectPain = () => {
         <div className='container flex flex-row absolute'>
             {!ruqAbsSelected ? (<img src={PainsData.absPain.absImg.absRightUp} alt="Ruq Pain" className='relative opacity-0'/>) : 
             (<div>
-                <img src={PainsData.absPain.absMes.absRightUpMes} alt='Ruq Pain'className='absolute 2xl:top-[68px] xl:top-[65px] md:top-[50px] sm:top-[50px]'/>
+                <img src={PainsData.absPain.absMes.absRightUpMes} alt='Ruq Pain'className='absolute 2xl:top-[60px] xl:top-[60px] lg:top-[50px] md:top-[50px] sm:top-[50px] xs:top-[28px] fold:top-[20px]'/>
                 <img src={PainsData.absPain.absImg.absRightUp} alt="Ruq Pain" className='relative' /> 
             </div>)}
         </div>
@@ -71,7 +71,7 @@ const AbsSelectPain = () => {
         <div className='container flex flex-row absolute'>
             {!rlqAbsSelected ? (<img src={PainsData.absPain.absImg.absRightLow} alt="Rlq Pain" className='relative opacity-0'/>) : 
             (<div>
-                <img src={PainsData.absPain.absMes.absRightLowMes} alt='Rlq Pain'className='absolute 2xl:-top-[55px] xl:-top-[50px] md:-top-[28px] sm:-top-[28px] '/>
+                <img src={PainsData.absPain.absMes.absRightLowMes} alt='Rlq Pain'className='absolute 2xl:-top-[65px] xl:-top-[60px] md:-top-[40px] sm:-top-[40px] xs:-top-[30px] fold:-top-[15px]'/>
                 <img src={PainsData.absPain.absImg.absRightLow} alt="Rlq Pain" className='relative' /> 
             </div>)}
         </div>
@@ -87,38 +87,73 @@ const AbsSelectPain = () => {
         <div className='container flex flex-row '>
             {!bottomAbsSelected ? (<img src={PainsData.absPain.absImg.absBottom} alt="Suprapubic Pain" className='relative opacity-0'/>) : 
             (<div>
-                <img src={PainsData.absPain.absMes.absBottomMes} alt="Suprapubic Pain"className='absolute top-[20px] left-[10px]'/>
+                <img src={PainsData.absPain.absMes.absBottomMes} alt="Suprapubic Pain"className='absolute top-[10px]  sm:left-[10px] xs:left-[5px] fold:left-[0px]'/>
                 <img src={PainsData.absPain.absImg.absBottom} alt="Suprapubic Pain" className='relative' /> 
             </div>)}
         </div>
         
-        <button type='button' className='absolute top-[204px] right-[220px] px-[28px] p-[27px] 
+        {/* Epigastrium Pain Btn */}
+        <button type='button' className='absolute top-[204px] right-[220px] px-[28px] p-[27px]
+        fold:top-[85px] fold:right-[92px] fold:px-[11px] fold:p-[11px]
+        xs:top-[128px] xs:right-[140px] xs:px-[16px] xs:p-[16px]
+        sm:top-[200px] sm:right-[220px] sm:px-[28px] sm:p-[28px]
+        lg:top-[200px] lg:right-[220px] lg:px-[28px] lg:p-[28px] 
         xl:top-[258px] xl:right-[280px] xl:px-[35px] xl:p-[35px] 
-        2xl:top-[270px] 2xl:right-[298px] 2xl:p-[36px] rotate-45 rounded-xl' onClick={handleTopAbs}/>
+        2xl:top-[270px] 2xl:right-[298px] 2xl:p-[36px] rotate-45 rounded-xl bg-red-400' onClick={handleTopAbs}/>
 
+        {/* Luq Pain Btn */}
         <button type='button' className='absolute top-[242px] right-[175px] px-[27px] p-[26px] 
-        xl:top-[308px] xl:right-[220px] xl:px-[36px] xl:p-[32px] 2xl:top-[322px] 
-        2xl:right-[230px] 2xl:px-[38px] 2xl:p-[35px] rotate-45 rounded-full' onClick={handleLuqAbs}/>
+        fold:top-[100px] fold:right-[72px] fold:px-[11px] fold:p-[11px]
+        xs:top-[150px] xs:right-[110px] xs:px-[16px] xs:p-[16px]
+        sm:top-[240px] sm:right-[170px] sm:px-[28px] sm:p-[28px]
+        lg:top-[235px] lg:right-[170px] lg:px-[28px] lg:p-[28px] 
+        xl:top-[300px] xl:right-[220px] xl:px-[35px] xl:p-[35px] 
+        2xl:top-[320px] 2xl:right-[230px] 2xl:px-[38px] 2xl:p-[35px] rotate-45 rounded-full  bg-red-400' onClick={handleLuqAbs}/>
 
+        {/* Llq Pain Btn */}
         <button type='button' className='absolute top-[305px] right-[175px] px-[26px] p-[27px] 
-        xl:top-[385px] xl:right-[223px] xl:px-[32px] xl:p-[33px] 2xl:top-[405px] 
-        2xl:right-[235px] 2xl:px-[32px] 2xl:p-[36px] rotate-45 rounded-3xl' onClick={handleLlqAbs}/>
+        fold:top-[128px] fold:right-[73px] fold:px-[11px] fold:p-[11px]
+        xs:top-[190px] xs:right-[110px] xs:px-[16px] xs:p-[16px]
+        sm:top-[302px] sm:right-[170px] sm:px-[28px] sm:p-[28px]
+        lg:top-[305px] lg:right-[170px] lg:px-[26px] lg:p-[26px] 
+        xl:top-[385px] xl:right-[220px] xl:px-[33px] xl:p-[33px] 
+        2xl:top-[405px] 2xl:right-[235px] 2xl:px-[32px] 2xl:p-[36px] rotate-45 rounded-3xl  bg-red-400' onClick={handleLlqAbs}/>
 
+        {/* Ruq Pain Btn */}        
         <button type='button' className='absolute top-[242px] right-[270px] px-[27px] p-[26px] 
-        xl:top-[308px] xl:right-[340px] xl:px-[36px] xl:p-[32px] 2xl:top-[322px] 
-        2xl:right-[355px] 2xl:px-[38px] 2xl:p-[35px] -rotate-45 rounded-full' onClick={handleRuqAbs}/>
+        fold:top-[100px] fold:right-[113px] fold:px-[11px] fold:p-[11px]
+        xs:top-[150px] xs:right-[170px] xs:px-[16px] xs:p-[16px]
+        sm:top-[240px] sm:right-[270px] sm:px-[28px] sm:p-[28px]
+        lg:top-[235px] lg:right-[270px] lg:px-[28px] lg:p-[28px] 
+        xl:top-[300px] xl:right-[340px] xl:px-[35px] xl:p-[35px] 
+        2xl:top-[320px] 2xl:right-[355px] 2xl:px-[38px] 2xl:p-[35px] -rotate-45 rounded-full  bg-red-400' onClick={handleRuqAbs}/>
         
+        {/* Rlq Pain Btn */}
         <button type='button' className='absolute top-[305px] right-[275px] px-[26px] p-[27px] 
-        xl:top-[370px] xl:right-[345px] xl:px-[32px] xl:p-[33px] 2xl:top-[405px] 
-        2xl:right-[365px] 2xl:px-[32px] 2xl:p-[36px] -rotate-45 rounded-3xl' onClick={handleRlqAbs}/>
+        fold:top-[128px] fold:right-[113px] fold:px-[11px] fold:p-[11px]
+        xs:top-[190px] xs:right-[170px] xs:px-[16px] xs:p-[16px]
+        sm:top-[302px] sm:right-[270px] sm:px-[28px] sm:p-[28px]
+        lg:top-[305px] lg:right-[270px] lg:px-[26px] lg:p-[26px] 
+        xl:top-[385px] xl:right-[340px] xl:px-[33px] xl:p-[33px] 
+        2xl:top-[405px] 2xl:right-[365px] 2xl:px-[32px] 2xl:p-[36px] -rotate-45 rounded-3xl  bg-red-400' onClick={handleRlqAbs}/>
 
+        {/* Umbilicus Pain Btn */}        
         <button type='button' className='absolute top-[275px] right-[225px] px-[24px] p-[24px] 
-        xl:top-[350px] xl:right-[286px] xl:px-[30px] xl:p-[30px] 2xl:top-[369px] 
-        2xl:right-[300px] 2xl:px-[32px] 2xl:p-[32px] -rotate-45 rounded-full' onClick={handleMidAbs}/>
+        fold:top-[115px] fold:right-[93px] fold:px-[10px] fold:p-[10px]
+        xs:top-[170px] xs:right-[140px] xs:px-[16px] xs:p-[16px]
+        sm:top-[272px] sm:right-[224px] sm:px-[25px] sm:p-[25px]
+        lg:top-[272px] lg:right-[223px] lg:px-[25px] lg:p-[25px] 
+        xl:top-[345px] xl:right-[280px] xl:px-[33px] xl:p-[33px] 
+        2xl:top-[370px] 2xl:right-[300px] 2xl:px-[32px] 2xl:p-[32px] -rotate-45 rounded-full  bg-red-400' onClick={handleMidAbs}/>
 
+        {/* Suprapubic Pain Btn */}
         <button type='button' className='absolute top-[335px] right-[222px] px-[25px] p-[27px] 
+        fold:top-[140px] fold:right-[92px] fold:px-[11px] fold:p-[11px]
+        xs:top-[210px] xs:right-[140px] xs:px-[16px] xs:p-[16px]
+        sm:top-[335px] sm:right-[220px] sm:px-[28px] sm:p-[28px]
+        lg:top-[330px] lg:right-[220px] lg:px-[28px] lg:p-[28px] 
         xl:top-[425px] xl:right-[280px] xl:px-[33px] xl:p-[33px] 
-        2xl:top-[448px] 2xl:right-[295px] 2xl:p-[34px] -rotate-45 rounded-3xl' onClick={handleBottomAbs}/>
+        2xl:top-[450px] 2xl:right-[295px] 2xl:p-[34px] -rotate-45 rounded-3xl  bg-red-400' onClick={handleBottomAbs}/>
 
     </div>
   )
--- src/pages/FingersPage.js
+++ src/pages/FingersPage.js
@@ -30,20 +30,14 @@ const FingersPage = () => {
                     <img src={othersPain} alt="All Abs Pain"/>
                   </div>
                 )}
-                {/* <button type='button' className='absolute top-[506px] right-[80px] px-[163px] p-[22px] 
-                xl:top-[643px] xl:right-[100px] xl:p-[28px] xl:px-[210px] 
-                2xl:top-[676px] 2xl:right-[109px] 2xl:p-[30px] 2xl:px-[218px] bg-transparent border-gray-300 rounded-full' onClick={handleAllPain}/> */}
 
+                {/* All Pain Button */}
                 <button type='button' className='absolute top-[500px] right-[174px] pl-[75px] pr-[75px] p-[22px]
                 fold:top-[208px] fold:right-[35px] fold:px-[70px] fold:p-[10px] xs:top-[315px] xs:right-[52px] xs:px-[102px] xs:p-[14px]
                 lg:top-[505px] lg:right-[80px] lg:p-[23px] lg:px-[165px] sm:top-[505px] sm:right-[80px] sm:p-[23px] sm:px-[165px] xl:top-[640px] xl:right-[100px] xl:p-[30px] xl:px-[210px] 
                 2xl:top-[676px] 2xl:right-[100px] 2xl:p-[30px] 2xl:px-[225px] border-gray-300 rounded-full' onClick={handleAllPain}/>
 
-                {/* absolute top-[496px] right-[174px] pl-[75px] pr-[75px] p-[22px]
-                fold:top-[240px] fold:right-[70px] fold:px-[50px] fold:p-[15px] xs:top-[310px] xs:right-[105px] 
-                lg:top-[495px] lg:right-[175px] lg:p-[22px] lg:px-[80px] sm:top-[495px] sm:right-[170px] sm:p-[23px] sm:px-[80px] xl:top-[628px] xl:right-[220px] xl:p-[28px] xl:px-[95px] 
-                2xl:top-[660px] 2xl:right-[235px] 2xl:p-[30px] 2xl:px-[100px] border-gray-300 rounded-full */}
-            </div>
+        </div>
         </div>
         <div className='container flex flex-col mt-5'>
           <NextButton />
