00026d5582504b25a8e1d0d54e696de5ac2cff8d
Thananyasih <thananya.sih@gmail.com>
2023-12-27 16:25:55 +0700

    edit finger others pain button

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -6,6 +6,7 @@ import {useNavigate} from 'react-router-dom'
 
 const NextButton = () => {
 
+    const nextText = 'ต่อไป'
     const navigate = useNavigate();
 
     const navigateToFingerpage = (e) => {
@@ -14,7 +15,7 @@ const NextButton = () => {
     };
 
   return (
-    <button type='button' className=" text-gray-300 text-md font-sans text-center bg-gray-200 p-2 rounded-xl hover:text-gray-100 hover:bg-gray-400 hover:delay-50 hover:shadow-md" onClick={navigateToFingerpage}>ต่อไป</button>
+    <button type='button' className=" text-gray-300 text-md font-sans text-center bg-gray-200 p-2 rounded-xl hover:text-gray-100 hover:bg-gray-400 hover:delay-50 hover:shadow-md" onClick={navigateToFingerpage}>{nextText}</button>
   )
 }
 
