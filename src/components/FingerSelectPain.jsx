1136b97e38723e69e0e22130b589d4fa06deee5c
Thananyasih <thananya.sih@gmail.com>
2023-12-27 12:18:28 +0700

    edit files

--- src/components/FingerSelectPain.jsx
+++ src/components/FingerSelectPain.jsx
@@ -6,13 +6,13 @@ const FingerSelectPain = () => {
     const [midFingerSelected, setMidFingerSelected] = useState(false)
     const [bottomFingerSelected, setBottomFingerSelected] = useState(false)
 
-    const handleTopFinger = () => {
+    const handleTopFingerClick = () => {
         setTopFingerSelected(topFingerSelected => !topFingerSelected);
     }
-    const handleMidFinger = () => {
+    const handleMidFingerClick = () => {
         setMidFingerSelected(midFingerSelected => !midFingerSelected);
     }
-    const handleBottomFinger = () => {
+    const handleBottomFingerClick = () => {
         setBottomFingerSelected(bottomFingerSelected => !bottomFingerSelected);
     }
 
@@ -49,7 +49,7 @@ const FingerSelectPain = () => {
         sm:top-[130px] sm:right-[355px] sm:px-[14px] sm:p-[14px]
         lg:top-[130px] lg:right-[355px] lg:px-[14px] lg:p-[14px] 
         xl:top-[168px] xl:right-[445px] xl:px-[20px] xl:p-[15px]
-        2xl:top-[180px] 2xl:right-[470px] 2xl:px-[18px] 2xl:p-[14px] -rotate-45 rounded-ful' onClick={handleTopFinger}/>
+        2xl:top-[180px] 2xl:right-[470px] 2xl:px-[18px] 2xl:p-[14px] -rotate-45 rounded-ful' onClick={handleTopFingerClick}/>
 
         <button type='button' className='absolute top-[75px] right-[290px] px-[16px] p-[10px] 
         fold:top-[29px] fold:right-[122px] fold:px-[5px] fold:p-[5px]
@@ -57,7 +57,7 @@ const FingerSelectPain = () => {
         sm:top-[75px] sm:right-[290px] sm:px-[14px] sm:p-[14px]
         lg:top-[70px] lg:right-[290px] lg:px-[14px] lg:p-[14px] 
         xl:top-[90px] xl:right-[370px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[100px] 2xl:right-[390px] 2xl:px-[18px] 2xl:p-[14px] rounded-ful' onClick={handleTopFinger}/>
+        2xl:top-[100px] 2xl:right-[390px] 2xl:px-[18px] 2xl:p-[14px] rounded-ful' onClick={handleTopFingerClick}/>
 
         <button type='button' className='absolute top-[50px] right-[240px] px-[16px] p-[10px] 
         fold:top-[20px] fold:right-[100px] fold:px-[5px] fold:p-[5px]
@@ -65,7 +65,7 @@ const FingerSelectPain = () => {
         sm:top-[45px] sm:right-[240px] sm:px-[14px] sm:p-[14px]
         lg:top-[45px] lg:right-[240px] lg:px-[14px] lg:p-[14px] 
         xl:top-[60px] xl:right-[305px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[68px] 2xl:right-[322px] 2xl:px-[20px] 2xl:p-[14px] rounded-ful' onClick={handleTopFinger}/>
+        2xl:top-[68px] 2xl:right-[322px] 2xl:px-[20px] 2xl:p-[14px] rounded-ful' onClick={handleTopFingerClick}/>
 
         <button type='button' className='absolute top-[65px] right-[180px] px-[16px] p-[10px] 
         fold:top-[25px] fold:right-[76px] fold:px-[5px] fold:p-[5px]
@@ -73,7 +73,7 @@ const FingerSelectPain = () => {
         sm:top-[60px] sm:right-[180px] sm:px-[14px] sm:p-[14px]
         lg:top-[55px] lg:right-[185px] lg:px-[14px] lg:p-[14px] 
         xl:top-[80px] xl:right-[228px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[85px] 2xl:right-[245px] 2xl:px-[19px] 2xl:p-[14px] rounded-ful' onClick={handleTopFinger}/>
+        2xl:top-[85px] 2xl:right-[245px] 2xl:px-[19px] 2xl:p-[14px] rounded-ful' onClick={handleTopFingerClick}/>
 
         {/* Mid Finger Pain Selected button */}
         <button type='button' className='absolute top-[176px] right-[334px] px-[14px] p-[10px] 
@@ -82,7 +82,7 @@ const FingerSelectPain = () => {
         sm:top-[170px] sm:right-[330px] sm:px-[14px] sm:p-[14px]
         lg:top-[170px] lg:right-[332px] lg:px-[14px] lg:p-[14px] 
         xl:top-[220px] xl:right-[420px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[235px] 2xl:right-[448px] 2xl:px-[18px] 2xl:p-[14px] -rotate-12 rounded-full' onClick={handleMidFinger}/>
+        2xl:top-[235px] 2xl:right-[448px] 2xl:px-[18px] 2xl:p-[14px] -rotate-12 rounded-full' onClick={handleMidFingerClick}/>
 
         <button type='button' className='absolute top-[133px] right-[285px] px-[16px] p-[10px] 
         fold:top-[53px] fold:right-[118px] fold:px-[6px] fold:p-[6px]
@@ -90,7 +90,7 @@ const FingerSelectPain = () => {
         sm:top-[128px] sm:right-[285px] sm:px-[14px] sm:p-[14px]
         lg:top-[130px] lg:right-[285px] lg:px-[14px] lg:p-[14px] 
         xl:top-[165px] xl:right-[360px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[178px] 2xl:right-[380px] 2xl:px-[22px] 2xl:p-[14px] -rotate-12 rounded-full' onClick={handleMidFinger}/>
+        2xl:top-[178px] 2xl:right-[380px] 2xl:px-[22px] 2xl:p-[14px] -rotate-12 rounded-full' onClick={handleMidFingerClick}/>
 
         <button type='button' className='absolute top-[110px] right-[235px] px-[16px] p-[12px] 
         fold:top-[45px] fold:right-[98px] fold:px-[6px] fold:p-[6px]
@@ -98,7 +98,7 @@ const FingerSelectPain = () => {
         sm:top-[110px] sm:right-[235px] sm:px-[14px] sm:p-[14px]
         lg:top-[110px] lg:right-[240px] lg:px-[14px] lg:p-[14px] 
         xl:top-[140px] xl:right-[300px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[150px] 2xl:right-[314px] 2xl:px-[22px] 2xl:p-[14px] rounded-full' onClick={handleMidFinger}/>
+        2xl:top-[150px] 2xl:right-[314px] 2xl:px-[22px] 2xl:p-[14px] rounded-full' onClick={handleMidFingerClick}/>
 
         <button type='button' className='absolute top-[120px] right-[180px] px-[17px] p-[10px] 
         fold:top-[48px] fold:right-[76px] fold:px-[6px] fold:p-[6px]
@@ -106,7 +106,7 @@ const FingerSelectPain = () => {
         sm:top-[115px] sm:right-[185px] sm:px-[14px] sm:p-[14px]
         lg:top-[114px] lg:right-[182px] lg:px-[14px] lg:p-[14px] 
         xl:top-[150px] xl:right-[230px] xl:px-[20px] xl:p-[15px] 
-        2xl:top-[160px] 2xl:right-[240px] 2xl:px-[23px] 2xl:p-[14px] rounded-full' onClick={handleMidFinger}/>
+        2xl:top-[160px] 2xl:right-[240px] 2xl:px-[23px] 2xl:p-[14px] rounded-full' onClick={handleMidFingerClick}/>
 
         <button type='button' className='absolute top-[230px] right-[84px] px-[14px] p-[12px] 
         fold:top-[95px] fold:right-[35px] fold:px-[6px] fold:p-[6px]
@@ -114,7 +114,7 @@ const FingerSelectPain = () => {
         sm:top-[225px] sm:right-[80px] sm:px-[14px] sm:p-[14px]
         lg:top-[225px] lg:right-[85px] lg:px-[14px] lg:p-[14px] 
         xl:top-[290px] xl:right-[100px] xl:px-[20px] xl:p-[15px]  
-        2xl:top-[305px] 2xl:right-[112px] 2xl:px-[20px] 2xl:p-[16px] rotate-45 rounded-full' onClick={handleMidFinger}/>
+        2xl:top-[305px] 2xl:right-[112px] 2xl:px-[20px] 2xl:p-[16px] rotate-45 rounded-full' onClick={handleMidFingerClick}/>
 
         {/* Bottom Finger Pain Selected button */}
         <button type='button' className='absolute top-[226px] right-[306px] px-[19px] p-[14px] 
@@ -123,7 +123,7 @@ const FingerSelectPain = () => {
         sm:top-[225px] sm:right-[305px] sm:px-[20px] sm:p-[14px]
         lg:top-[228px] lg:right-[305px] lg:px-[20px] lg:p-[14px] 
         xl:top-[280px] xl:right-[390px] xl:px-[25px] xl:p-[20px]  
-        2xl:top-[302px] 2xl:right-[409px] 2xl:px-[26px] 2xl:p-[18px] -rotate-12 rounded-full' onClick={handleBottomFinger}/>
+        2xl:top-[302px] 2xl:right-[409px] 2xl:px-[26px] 2xl:p-[18px] -rotate-12 rounded-full' onClick={handleBottomFingerClick}/>
 
         <button type='button' className='absolute top-[133px] right-[285px] px-[16px] p-[10px] 
         fold:top-[86px] fold:right-[110px] fold:px-[8px] fold:p-[6px]
@@ -131,7 +131,7 @@ const FingerSelectPain = () => {
         sm:top-[210px] sm:right-[265px] sm:px-[20px] sm:p-[14px]
         lg:top-[210px] lg:right-[270px] lg:px-[20px] lg:p-[14px] 
         xl:top-[260px] xl:right-[340px] xl:px-[25px] xl:p-[20px] 
-        2xl:top-[278px] 2xl:right-[358px] 2xl:px-[26px] 2xl:p-[18px] -rotate-12 rounded-full ' onClick={handleBottomFinger}/>
+        2xl:top-[278px] 2xl:right-[358px] 2xl:px-[26px] 2xl:p-[18px] -rotate-12 rounded-full ' onClick={handleBottomFingerClick}/>
 
         <button type='button' className='absolute top-[110px] right-[235px] px-[16px] p-[12px] 
         fold:top-[80px] fold:right-[93px] fold:px-[8px] fold:p-[6px]
@@ -139,7 +139,7 @@ const FingerSelectPain = () => {
         sm:top-[200px] sm:right-[225px] sm:px-[20px] sm:p-[14px]
         lg:top-[200px] lg:right-[225px] lg:px-[20px] lg:p-[14px] 
         xl:top-[250px] xl:right-[285px] xl:px-[25px] xl:p-[20px] 
-        2xl:top-[260px] 2xl:right-[300px] 2xl:px-[27px] 2xl:p-[18px] rounded-full' onClick={handleBottomFinger}/>
+        2xl:top-[260px] 2xl:right-[300px] 2xl:px-[27px] 2xl:p-[18px] rounded-full' onClick={handleBottomFingerClick}/>
 
         <button type='button' className='absolute top-[120px] right-[180px] px-[17px] p-[10px] 
         fold:top-[80px] fold:right-[76px] fold:px-[8px] fold:p-[6px]
@@ -147,7 +147,7 @@ const FingerSelectPain = () => {
         sm:top-[200px] sm:right-[180px] sm:px-[20px] sm:p-[14px]
         lg:top-[195px] lg:right-[182px] lg:px-[20px] lg:p-[14px] 
         xl:top-[250px] xl:right-[230px] xl:px-[25px] xl:p-[20px] 
-        2xl:top-[263px] 2xl:right-[243px] 2xl:px-[26px] 2xl:p-[18px] rotate-12 rounded-full' onClick={handleBottomFinger}/>
+        2xl:top-[263px] 2xl:right-[243px] 2xl:px-[26px] 2xl:p-[18px] rotate-12 rounded-full' onClick={handleBottomFingerClick}/>
 
         <button type='button' className='absolute top-[230px] right-[84px] px-[14px] p-[12px] 
         fold:top-[120px] fold:right-[48px] fold:px-[8px] fold:p-[6px]
@@ -155,7 +155,7 @@ const FingerSelectPain = () => {
         sm:top-[295px] sm:right-[110px] sm:px-[20px] sm:p-[14px]
         lg:top-[290px] lg:right-[120px] lg:px-[20px] lg:p-[14px] 
         xl:top-[370px] xl:right-[150px] xl:px-[25px] xl:p-[20px] 
-        2xl:top-[392px] 2xl:right-[155px] 2xl:px-[26px] 2xl:p-[18px] rotate-45 rounded-full ' onClick={handleBottomFinger}/>
+        2xl:top-[392px] 2xl:right-[155px] 2xl:px-[26px] 2xl:p-[18px] rotate-45 rounded-full ' onClick={handleBottomFingerClick}/>
     </div>
   )
 }
--- src/pages/AbsPage.js
+++ src/pages/AbsPage.js
@@ -7,18 +7,19 @@ import AbsSelectPain from '../components/AbsSelectPain'
 
 
 const AbsPage = () => {
-
-    const [isAllPain, setIsAllPain] = useState(false)
+  
+  const AbsQuestion = 'ปวดท้องที่บริเวณใดมากที่สุด?'
+  const [isAllPain, setIsAllPain] = useState(false)
      
-    const handleAllPain = () => {
+  const handleAllPainClick = () => {
       setIsAllPain(isAllPain => !isAllPain);
-    }
+  }
   
   return (
     <div className='m-5 mt-3'>
       <div className="container relative flex flex-col text-center top-0 max-h-full mac:max-w-macScreen sm:max-w-smScreen md:max-w-mdScreen lg:max-w-lgScreen xl:max-w-xlScreen 2xl:max-w-fullScreen z-50">
         <div className="justify-items-center flex flex-col border-solid:-2:-gray-300 rounded-3xl shadow-3xl">
-          <h1 className="sm:text-sm md:text-md lg:text-lg xl:text-xl font-sans m-5 mt-8 mb-0 text-gray-600">ปวดท้องที่บริเวณใดมากที่สุด?</h1>
+          <h1 className="sm:text-sm md:text-md lg:text-lg xl:text-xl font-sans m-5 mt-8 mb-0 text-gray-600">{AbsQuestion}</h1>
             <div className='flex flex-col relative items-center'>
               <img src={absImage} alt="Default Abs" className='relative'/>
 
@@ -34,7 +35,7 @@ const AbsPage = () => {
                 <button type='button' className='absolute top-[496px] right-[174px] pl-[75px] pr-[75px] p-[22px]
                 fold:top-[240px] fold:right-[70px] fold:px-[50px] fold:p-[15px] xs:top-[310px] xs:right-[105px] 
                 lg:top-[495px] lg:right-[175px] lg:p-[22px] lg:px-[80px] sm:top-[495px] sm:right-[170px] sm:p-[23px] sm:px-[80px] xl:top-[628px] xl:right-[220px] xl:p-[28px] xl:px-[95px] 
-                2xl:top-[660px] 2xl:right-[235px] 2xl:p-[30px] 2xl:px-[100px] border-gray-300 rounded-full' onClick={handleAllPain}/>
+                2xl:top-[660px] 2xl:right-[235px] 2xl:p-[30px] 2xl:px-[100px] border-gray-300 rounded-full' onClick={handleAllPainClick}/>
                 
             </div>
         </div>
